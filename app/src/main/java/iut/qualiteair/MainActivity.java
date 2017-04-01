dda05ab9bedcf8b9c4719bcb3708d4fe08b60f49
amanda07511 <amanda07511@gmail.com>
2017-04-01 14:52:01 +0200

    Added Models  for map the object   that we receive in  a serch of la API, Added database structure and helper for  stoker cities, and added SearchActivity  for looking  a city by the name in the API and save it in the database

--- app/src/main/java/iut/qualiteair/MainActivity.java
+++ app/src/main/java/iut/qualiteair/MainActivity.java
@@ -5,12 +5,38 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import iut.qualiteair.models.GlobalObject;
+import iut.qualiteair.tools.MyAdapter;
+import iut.qualiteair.tools.db.DatabaseHandler;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private MyAdapter mAdapter;
+    private DatabaseHandler database;
+    private ArrayList<String> myDataset = new ArrayList<>();
+    private ArrayList<GlobalObject> initialCities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +55,34 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(this, initialCities);
+        mRecyclerView.setAdapter(mAdapter);
+
+        database = new DatabaseHandler(this);
+        database.open();
+        List<GlobalObject> tmpCities = database.getAllObj();
+        database.close();
+
+        for (int i = 0; i < tmpCities.size(); i++) {
+            myDataset.add("@" + tmpCities.get(i).getRxs().getObs().get(0).getMsg().getCity().getIdx());
+        }
+
+        for (int i = 0; i < myDataset.size(); i++) {
+            getDataFromUrl("https://api.waqi.info/api/feed/" + myDataset.get(i) + "/obs.fr.json?token=af073d16e3707f6d085660cfcd0137a61b961365");
+        }
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,4 +105,27 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void getDataFromUrl(String url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Gson gson = new Gson();
+                        GlobalObject obj = gson.fromJson(response, GlobalObject.class);
+                        System.out.print("AQUI ESTA--------->"+obj.getRxs().getObs().get(0).getMsg().getCity().getName());
+                        initialCities.add(obj);
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(stringRequest);
+    }//end getDATA
 }
